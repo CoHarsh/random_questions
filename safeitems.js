3e48c30333a7c9be38fd17029a0df8e2fe46f329
Harsh Prajapati <harshmp492@gmail.com>
2023-03-26 16:03:00 +0530

    updated code from github BUG

--- safeitems.js
+++ safeitems.js
@@ -1,2 +1,2 @@
 var BACKEND_URI_LOCAL = 'http://localhost:3000';
-// var BACKEND_URI = "https://solvemorequestions-backend.onrender.com"
+var BACKEND_URI = "https://solvemorequestions-backend.onrender.com"
